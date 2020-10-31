e064bb7c5dea4ef597414a385c6d8934c503eccb
Daniel Ward <danielaj_28@hotmail.co.uk>
2020-10-31 01:31:54 +0000

    modularising webhooks

--- modules/webhooks.js
+++ modules/webhooks.js
@@ -1,17 +1,8 @@
 module.exports.setup = function (app) {
-  app.get("/webhook/", (req, res) => {
-
-    console.log("Request received");
-    res.status(200);
-    res
-    .status(200)
-    .json(
-      "Webhook Triggered"
-    );
-
+  function RunScript(ScriptPath) {
     var spawn = require("child_process").spawn,
       child;
-    child = spawn("powershell.exe", ["c:\\temp\\pstest.ps1"]);
+    child = spawn("powershell.exe", [ScriptPath]);
     child.stdout.on("data", function (data) {
       console.log("Powershell Data: " + data);
     });
@@ -22,7 +13,24 @@ module.exports.setup = function (app) {
       console.log("Powershell Script finished");
     });
     child.stdin.end(); //end input
-    
+  }
+
+  app.post("/webhook/:token", (req, res) => {
+    console.log("Request received");
+
+    switch (req.params.token) {
+      case "pKgcNHuIAHnMg5lGeGxJvMTY1IRV0xkxNaWTVAqddyBLRcH1uP8U2zWDG8Z8vRcgTDuV1c674vz01MX7hPiR0t9HXDpmnC2l4eSz":
+        RunScript("c:\\temp\\pstest.ps1");
+        break;
+
+      default:
+        res.status(404).json("Webhook Not Found");
+        console.error("Access token was not recognised: ",req.params.token);
+        return;
+    }
+
+    res.status(200).json("Webhook Triggered");
+
     console.log("Powershell passed");
   });
   return;
